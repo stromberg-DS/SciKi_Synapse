3e1d2052eb4520f775fee5660d3724f99a29a46f
DStromberg <strombergundy@gmail.com>
2024-08-20 16:32:52 -0600

    LED strip working.

--- src/SciKi_Synapse.cpp
+++ src/SciKi_Synapse.cpp
@@ -12,13 +12,50 @@
 SYSTEM_MODE(AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
 
-const int PIXEL_COUNT = 144;
+const int PIXEL_COUNT = 190;
 const int BUTTON_PIN = D0;
+const int FRAME_DELAY = 20;
+
+int brightness = 100;
+int lastMillis = 0;
+int currentMillis;
+
+Adafruit_NeoPixel pixel(PIXEL_COUNT, SPI1, WS2812);
+Button dopamineButton(BUTTON_PIN);
 
 void setup() {
+    Serial.begin(9600);
+    pixel.begin();
+    pixel.setBrightness(brightness);
+
+    for(int i=0; i<PIXEL_COUNT; i++){
+        pixel.setPixelColor(i, 0x00FF00);
+        pixel.show();
+        delay(10);
+    }
+
+    delay(2000);
+    pixel.clear();
+    pixel.show();
+    
 
 }
 
 void loop() {
+    static int currentLED = 0;
+    currentMillis = millis();
+
+    if((currentMillis - lastMillis) > FRAME_DELAY){
+        if (currentLED < PIXEL_COUNT){
+            pixel.clear();
+            pixel.setPixelColor(currentLED, 0xFF0000);
+            currentLED++;
+
+        } else{
+            currentLED = 0;
+        }
+        lastMillis = currentMillis;
+        pixel.show();
+    }
 
 }
