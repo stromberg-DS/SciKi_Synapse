8342f71790c4416c387119b1da71e45a82d1a2e7
DStromberg <strombergundy@gmail.com>
2024-08-20 15:56:23 -0600

    Initial Commit

---
+++ src/SciKi_Synapse.cpp
@@ -0,0 +1,24 @@
+/* 
+ * Science of Kindness - Synapse
+ * Author: Daniel Stromberg
+ * Date: 8/20/24
+*/
+
+#include "Particle.h"
+#include <neopixel.h>
+#include <math.h>
+#include "Button.h"
+
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+const int PIXEL_COUNT = 144;
+const int BUTTON_PIN = D0;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
